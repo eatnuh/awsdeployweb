fe214507ec3cf1d69fa02cbc6861188a6b235158
eatnuh <eatnuh@gmail.com>
2022-11-06 02:40:19 +0900

    first commit

---
+++ src/main/java/com/eatnuh/AwsDeployWebApplication.java
@@ -0,0 +1,23 @@
+package com.eatnuh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class AwsDeployWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsDeployWebApplication.class, args);
+    }
+
+    @RestController
+    static class HelloController {
+        @RequestMapping
+        public String hello() {
+            return "hello";
+        }
+
+    }
+}
